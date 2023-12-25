1a6a9f227b39c978caf353b5c75fedb67327beff
hpanushan <hpanushan@gmail.com>
2023-12-25 00:03:31 -0500

    added list type to query result

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from graphene import Schema, ObjectType, String, Int, Field
+from graphene import Schema, ObjectType, String, Int, Field, List
 
 class CustomerType(ObjectType):
     id = Int()
@@ -7,6 +7,7 @@ class CustomerType(ObjectType):
 
 class Query(ObjectType):
     user = Field(CustomerType, id=Int(default_value = 1))
+    users_by_min_age = List(CustomerType, min_age=Int(default_value = 20))
 
     # dummy data store
     users = [
@@ -14,15 +15,21 @@ class Query(ObjectType):
         {"id": 2, "name": "Bob", "age": 30},
         {"id": 3, "name": "Cathy", "age": 40},
     ]
-    def resolve_user(self, info, id):
+    @staticmethod
+    def resolve_user(root, info, id):
         matched_users = [user for user in Query.users if user["id"] == id]
         return matched_users[0] 
+    
+    @staticmethod
+    def resolve_users_by_min_age(root, info, min_age):
+        matched_users = [user for user in Query.users if user["age"] >= min_age]
+        return matched_users
 
-schema = Schema(query=Query)
+schema = Schema(query=Query, auto_camelcase=True)
 
 graphql_query = '''
 query myquery {
-    user(id: 2) {
+    usersByMinAge(minAge: 34) {
         id
         name
         age
